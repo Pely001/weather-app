5dd17e56bdd45f7ef1c17ff4d5117cc19448e4b5
Pedro Matias <112733794+Pely001@users.noreply.github.com>
2025-04-22 20:47:02 -0300

    Adicionando geolocalização

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
+const KEY = "32976100515e39c6464b732c2b90fd75";
+const inputCity =  document.querySelector(".city-input");
+const searchButton = document.querySelector(".search-button");
+
+function clickButton() {
+    const input = document.querySelector(".city-input").value;
+    citySearch(input);
+}
+
+async function citySearch(city) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${KEY}&lang=pt_br&units=metric`;
+
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+        if (response.ok) {
+            updateWeatherInfo(data);
+        } else {
+            alert(`Não foi possível encontrar informações sobre a cidade: ${city}`);
+        }
+
+    } catch (error) {
+        console.error("Erro ao buscar dados da API:", error);
+        alert("Ocorreu um erro ao buscar a previsão do tempo.");
+    }
+}
+
+function updateWeatherInfo(data) {
+    document.querySelector(".city").innerText = "Tempo em " + data.name;
+    document.querySelector(".temp").innerText = Math.floor(data.main.temp) + "° C";
+    document.querySelector(".weather-text").innerText = data.weather[0].description;
+    document.querySelector(".humidity").innerText = "Umidade " + data.main.humidity + "%";
+    document.querySelector(".img-weather").src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+}
+
+inputCity.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        clickButton();
+        console.log("Enter key pressed");
+    }
+});
+
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
